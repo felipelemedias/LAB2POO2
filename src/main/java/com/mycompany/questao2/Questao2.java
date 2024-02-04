b3a1e46fc63eb4ab77d63a47dc4c442a398f7f4e
fleme <fleme@DESKTOP-PLBVS5T>
2024-02-04 15:48:22 -0300

    atualizaçáão codigo

--- src/main/java/com/mycompany/questao2/Questao2.java
+++ src/main/java/com/mycompany/questao2/Questao2.java
@@ -15,91 +15,92 @@ public class Questao2 {
         // Com o Singleto pego a instancia das fabricas tanto toyota quanto honda
         Toyota toyotaFactory = Toyota.getInstancia();
         Honda hondaFactory = Honda.getInstancia();
-        
+
         Scanner scanner = new Scanner(System.in);
         int op;
-        
-        do{
+
+        do {
             System.out.println("Escolha uma opção");
-            System.out.println("1. Escolher carro");
-            System.out.println("0. Sair do programa");
-            
-            System.out.println("Opção: ");
+            System.out.println("1 - Escolher carro");
+            System.out.println("0 - Sair do programa");
+
+            System.out.println("Insira: ");
             op = scanner.nextInt();
             scanner.nextLine();
-            
-            switch(op){
-                case 1:
+
+            switch (op) {
+                case 1 -> {
                     System.out.println("Escolha entre Toyota ou Honda: ");
                     String toyotaOrHonda = scanner.nextLine().toLowerCase();
-                    
-                    switch(toyotaOrHonda){
-                        case "toyota":
-                            System.out.println("Você escolheu um Toyota, dentre desses qual voce quer");
+
+                    switch (toyotaOrHonda) {
+                        case "toyota" -> {
+                            System.out.println("Você está na sessão Toyota, escolha um dos modelos(digite):");
                             System.out.println("1. Hilux");
                             System.out.println("2. Corolla");
                             System.out.println("3. Etios");
                             System.out.println("Opção: ");
                             String modeloDesejadoT = scanner.nextLine().toLowerCase();
                             IVehicle toyota = toyotaFactory.makeVehicle(modeloDesejadoT);
-                            
-                            if(toyota != null){
-                                switch(modeloDesejadoT){
-                                    case "hilux":
+
+                            if (toyota != null) {
+                                switch (modeloDesejadoT) {
+                                    case "hilux" -> {
                                         System.out.println("Você tem um " + modeloDesejadoT);
                                         toyota.start();
                                         toyota.stop();
                                         toyota.drive();
-                                        break;
-                                    case "corolla":
+                                    }
+                                    case "corolla" -> {
                                         System.out.println("Você tem um " + modeloDesejadoT);
                                         toyota.start();
                                         toyota.stop();
                                         toyota.drive();
-                                        break;
-                                    case "etios":
+                                    }
+                                    case "etios" -> {
                                         System.out.println("Você tem um " + modeloDesejadoT);
                                         toyota.start();
                                         toyota.stop();
                                         toyota.drive();
-                                        break;    
+                                    }
                                 }
                             }
-                            break;
-                        case "honda":
-                            System.out.println("Você escolheu um Honda, dentre desses qual você quer");
+                    }
+                        case "honda" -> {
+                            System.out.println("Você está na sessão Honda, escolha um dos modelos(digite):");
                             System.out.println("1. Civic");
                             System.out.println("2. City");
                             System.out.println("3. Fit");
                             String modeloDesejadoH = scanner.nextLine().toLowerCase();
                             IVehicle honda = hondaFactory.makeVehicle(modeloDesejadoH);
-                            
-                            if(honda != null){
-                                switch(modeloDesejadoH){
-                                    case "civic":
+
+                            if (honda != null) {
+                                switch (modeloDesejadoH) {
+                                    case "civic" -> {
                                         System.out.println("Você tem um " + modeloDesejadoH);
                                         honda.start();
                                         honda.stop();
                                         honda.drive();
-                                        break;
-                                    case "city":
+                                    }
+                                    case "city" -> {
                                         System.out.println("Você tem um " + modeloDesejadoH);
                                         honda.start();
                                         honda.stop();
                                         honda.drive();
-                                        break;
-                                    case "fit":
+                                    }
+                                    case "fit" -> {
                                         System.out.println("Você tem um " + modeloDesejadoH);
                                         honda.start();
                                         honda.stop();
                                         honda.drive();
                                         break;    
+                                    }
                                 }
                             }
-                    break;
                     }
-            break;
+                    }
+                }
             }
-        }while(op!=0);
+        } while (op != 0);
     }
 }
